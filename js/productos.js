ffe22234fd09031fa8e7f60299ad918a46f177b2
Jano Aguiar <janoaguiar97@gmail.com>
2025-03-07 16:20:15 -0300

    se cambia el nombre

--- js/productos.js
+++ js/productos.js
@@ -47,6 +47,10 @@ function mostrarProductos(el) {
     boton.innerText = "Comprar";
     boton.className = "button-prod"
 
+    boton.addEventListener("click", () => {
+        agregarAlCarrito(el);
+    });
+
     card.appendChild(imagen);
     card.appendChild(nombre);
     card.appendChild(precio);
@@ -60,3 +64,122 @@ productos.forEach(el => {
 });
 
 
+// CARRITO
+let carrito = [];
+
+
+function cargarCarrito() {
+    const carritoGuardado = localStorage.getItem(`carrito`);
+    if(carritoGuardado){
+        carrito = JSON.parse(carritoGuardado);
+    }
+}
+
+function guardarCarrito(){
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+}
+
+
+const carritoContainer = document.getElementById("carrito-container");
+const botonCarrito = document.querySelector(".button");
+
+botonCarrito.addEventListener("click", () => {
+    carritoContainer.style.display = carritoContainer.style.display === "none" ? "flex" : "none";
+});
+
+
+function agregarAlCarrito(producto) {
+    let productoEnCarrito = carrito.find((prod) => prod.id === producto.id);
+
+    if(productoEnCarrito){
+        productoEnCarrito = (productoEnCarrito.cantidad || 1) + 1;
+    }
+    else{
+        carrito.push({...producto, cantidad: 1});
+    }
+    
+    contadorCarrito();
+    mostrarCarrito();
+    guardarCarrito();
+}
+
+
+function mostrarCarrito(){
+    const productosCarrito = document.getElementById("productos-carrito");
+    productosCarrito.innerHTML = ""
+
+    let subtotal = 0;
+    const envio = 4000;
+
+    carrito.forEach((producto) =>{
+        const item = document.createElement("div");
+        item.className = "product";
+        
+        item.innerHTML = `
+        <div>
+            <span>${producto.nombre}</span>
+        </div>
+        <div class="quantity">
+            <button class="restar" data-id="${producto.id}">
+                <svg fill="none" viewBox="0 0 24 24" height="14" width="14" xmlns="http://www.w3.org/2000/svg">
+                <path stroke-linejoin="round" stroke-linecap="round" stroke-width="2.5" stroke="#47484b" d="M20 12L4 12"></path>
+                </svg>
+            </button>
+                <label>${producto.cantidad || 1}</label>
+            <button class="sumar" data-id="${producto.id}">
+                <svg fill="none" viewBox="0 0 24 24" height="14" width="14" xmlns="http://www.w3.org/2000/svg">
+                    <path stroke-linejoin="round" stroke-linecap="round" stroke-width="2.5" stroke="#47484b" d="M12 4V20M20 12H4"></path>
+                </svg>
+            </button>
+        </div>
+        <label class="price small">$${producto.precio * (producto.cantidad || 1)}</label>
+        `;
+
+        productosCarrito.appendChild(item);
+        subtotal += producto.precio * (producto.cantidad || 1);
+    });
+
+    document.getElementById("subtotal").innerText = `$${subtotal}`;
+    document.getElementById("total").innerText = subtotal + envio;
+
+    
+    const botonSumar = document.querySelectorAll(".sumar");
+    const botonRestar = document.querySelectorAll(".restar");
+
+    botonSumar.forEach((boton) => {
+        boton.addEventListener("click", () => modificarCantidad(boton.dataset.id, 1));
+    });
+
+    botonRestar.forEach((boton) => {
+        boton.addEventListener("click", () => modificarCantidad(boton.dataset.id, -1));
+    });
+}
+
+
+function contadorCarrito(){
+    const contador = document.querySelector(".quantity-prod p");
+    contador.innerText = carrito.length;
+}
+
+
+function modificarCantidad(id, cambio) {
+    const producto = carrito.find((prod) => prod.id == id);
+
+    if(producto){
+        if(producto.cantidad){
+            producto.cantidad += cambio;
+        }
+        else {
+            producto.cantidad = 1 + cambio;
+        }
+        if(producto.cantidad <= 0){
+            carrito = carrito.filter((prod) => prod.id != id);
+        }
+    }
+
+    contadorCarrito();
+    mostrarCarrito();
+    guardarCarrito();
+}
+
+cargarCarrito();
